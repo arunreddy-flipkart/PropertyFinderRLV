f93e6dc76812021544887291508fd15b0495fb99
Arun Kumar Reddy <arunkumar.kr@flipkart.com>
2019-12-23 11:42:17 +0530

    Changing itemContainer prop horizontal false

--- src/Components/SearchResults.js
+++ src/Components/SearchResults.js
@@ -152,7 +152,7 @@ class SearchResults extends React.Component {
               layoutProvider={this.layoutProvider}
               forceNonDeterministicRendering={true}
               itemContainer={FlexView}
-              itemContainerProps={{ horizontal : true }}
+              itemContainerProps={{ horizontal : false }}
               renderFooter={renderFooter}
             />
         </View>
